b0e05c5452f67d682bf88df1c320d706ba80c1cc
maxhoward <mhoward@g.hmc.edu>
2015-06-07 18:13:48 -0700

    working dice panel

--- dicePanel.py
+++ dicePanel.py
@@ -9,23 +9,28 @@ class Die():
         self.station = sType
         self.faces = self.getFaces(sType)
         self.faceUpSide = random.choice(self.faces)
-        self.imgNames = self.getImgNames(sType)
+        self.imgs = self.getImgs(sType)
         self.isLocked = False
-        self.widget = Label(parent, image = self.imgNames[self.faceUpSide])
+        self.widget = Label(parent, image = self.imgs[self.faceUpSide])
         self.widget.pack(side = RIGHT)
         self.widget.bind("<Button-1>", self.toggleLock)
 
 
     def toggleLock(self, event):
         self.isLocked = not self.isLocked
-        print "Locked", self.isLocked
 
-    def getFaces(self, station):
+    def getFaces(self, sType):
         return range(1,7)
 
-    def getImgNames(self, station):
-        images = ["one.gif","two.gif","three.gif","four.gif","five.gif","six.gif"]
+    def getImgs(self, sType):
+        imageNames = ["one.gif","two.gif","three.gif","four.gif","five.gif","six.gif"]
+        images = [PhotoImage(file = name) for name in imageNames] 
         return {x:images[x-1] for x in self.faces}
+
+    def reroll(self):
+        self.faceUpSide = random.choice(self.faces)
+        self.widget.configure(image = self.imgs[self.faceUpSide])
+
 
 
 
@@ -37,6 +42,8 @@ class DicePanel:
         self.frame.pack()
         self.diceCount = self.getCount(sType)
         self.dice = self.makeDice()
+        self.rollButton = Button(self.frame, text="Reroll", command=self.rollDice)
+        self.rollButton.pack(side = RIGHT)
 
     def getCount(self, sType):
         return 4
@@ -44,6 +51,10 @@ class DicePanel:
     def makeDice(self):
         return [Die(self.frame, self.sType) for x in range(self.diceCount)]
 
+    def rollDice(self):
+        for die in self.dice:
+            if not die.isLocked:
+                die.reroll()
 
 if __name__ == "__main__":
     root = Tk()
